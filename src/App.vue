7ac36d230a76e46d7584aa7758b121093a5ad83f
Jean Mendonça <pierrybm@gmail.com>
2021-02-01 14:43:44 -0300

    progress component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>Tasks</h1>
+    <TasksProgress :progress="progress" />
     <NewTask @taskAdded="addTask" />
     <Tasks
       :tasks="tasks"
@@ -13,13 +14,22 @@
 <script>
 import Tasks from "./components/Tasks.vue";
 import NewTask from "./components/NewTask.vue";
+import TasksProgress from "./components/TasksProgress.vue";
 
 export default {
-  components: { Tasks, NewTask },
+  components: { Tasks, NewTask, TasksProgress },
   data() {
     return {
       tasks: [],
     };
+  },
+  computed: {
+    progress() {
+      const total = this.tasks.length;
+      const done = this.tasks.filter((t) => !t.pending).length;
+
+      return Math.round((done / total) * 100) || 0;
+    },
   },
   methods: {
     addTask(task) {
