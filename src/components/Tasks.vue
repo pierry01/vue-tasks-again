9bfc21cffae3044d5c3085ddc1bdbc12bc706c23
Jean Mendonça <pierrybm@gmail.com>
2021-02-01 14:03:21 -0300

    tasks component 2

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="tasks">
-    <Task v-for="task in tasks" :key="task.title" :task="task" />
+    <template v-if="tasks.length">
+      <Task v-for="task in tasks" :key="task.title" :task="task" />
+    </template>
+
+    <p v-else class="no-task">Sua vida está em dia :)</p>
   </div>
 </template>
 
@@ -19,4 +23,19 @@ export default {
 </script>
 
 <style>
+.tasks {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.tasks .task {
+  margin: 10px;
+}
+
+.no-task {
+  color: #AAA;
+  font-size: 1.7rem;
+}
 </style>
